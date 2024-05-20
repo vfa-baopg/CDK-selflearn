05441068ae256631dfdb3ac229003691817d696f
vfa-baopg <baopg@vitalify.asia>
2024-05-20 13:51:12 +0700

    Initial commit

---
+++ test/cdk-selflearn.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSelflearn from '../lib/cdk-selflearn-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-selflearn-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSelflearn.CdkSelflearnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
