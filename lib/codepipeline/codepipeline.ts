17399f42a3b7114ba140a4cc961c646db9379ab8
doletu <102934755+vfa-tudl@users.noreply.github.com>
2024-06-15 22:46:29 +0700

    Adapt Stack in code

--- lib/codepipeline/codepipeline.ts
+++ lib/codepipeline/codepipeline.ts
@@ -1,89 +1,74 @@
-
 import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codepipelineActions from 'aws-cdk-lib/aws-codepipeline-actions';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
-import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import { StackProps } from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { S3Stack } from '../s3-stack';
+import { EC2Stack } from '../ec2-stack';
+import { ECSStack } from '../cluster-stack';
 
 export interface CodepipelineStackProps extends StackProps {
-  s3Bucket?: s3.Bucket,
-  ecrRepo?: ecr.Repository,
-  service?: ecs.FargateService
+  s3Stack?: S3Stack;
+  ec2?: EC2Stack;
+  ecrRepo?: ecr.Repository;
+  service?: ECSStack;
+  resource?: ('api' | 'web' | 'admin');
 }
 
 export class CodepipelineBuildDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: CodepipelineStackProps) {
     super(scope, id, props);
+
+    // Ensure props.resource is defined and not empty
+    const resource = props.resource ?? 'web';
+
     // Create S3 bucket if not provided
-    if (props.s3Bucket == undefined) {
-      props.s3Bucket = new s3.Bucket(this, 'SourceBucket', {
-        versioned: true, // Enable versioning for the bucket
-      });
+    if (props.s3Stack == undefined) {
+      props.s3Stack = new S3Stack(this);
     }
 
-    // Create an ECR repository if not Provided
-    if (props.ecrRepo == undefined){
+    // Create an ECR repository if not provided
+    if (props.ecrRepo == undefined) {
       props.ecrRepo = new ecr.Repository(this, 'EcrRepository');
     }
-   
-    //Create ECS Service
-    if (props.service == undefined){
-      // Create ECS cluster
-      const cluster = new ecs.Cluster(this, 'EcsCluster', {
-        vpc: new ec2.Vpc(this, 'Vpc', { maxAzs: 3 }) // Default is all AZs in the region
-      });
 
-      // Create ECS task definition
-      const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef');
-      const container = taskDefinition.addContainer('web', {
-        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
-        memoryLimitMiB: 512,
-        cpu: 256,
-        logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'ecs' }),
-      });
-      container.addPortMappings({
-        containerPort: 80,
-      });
-
-      // Create ECS service
-      props.service = new ecs.FargateService(this, 'Service', {
-        cluster,
-        taskDefinition,
-        desiredCount: 1,
-      });                           
+    // Create EC2 stack if not provided
+    if (props.ec2 == undefined) {
+      props.ec2 = new EC2Stack(this);
     }
 
+    // Create ECS service if not provided
+    if (props.service == undefined) {
+      props.service = new ECSStack(this, props.ec2, props.s3Stack);
+    }
 
     // Create CodePipeline
-    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
-      pipelineName: "dev",
-    });
-    // Add S3 event as source action
-    const sourceOutput = new codepipeline.Artifact();
-    const sourceAction = new codepipelineActions.S3SourceAction({
-      actionName: 'Source_Stage',
-      bucket: props.s3Bucket,
-      bucketKey: 'src.zip', // Specify the path to the artifact within the bucket
-      output: sourceOutput, // Artifact where the source code will be stored
-      trigger: codepipelineActions.S3Trigger.EVENTS, // Trigger the pipeline on S3 events
+    const pipeline = new codepipeline.Pipeline(this, `${resource}-Pipeline`, {
+      pipelineName: `${resource}-dev`,
     });
 
-    // Add the source action to the pipeline
+    // Add source action (S3 event)
+    const sourceOutput = new codepipeline.Artifact();
+    const sourceAction = new codepipelineActions.S3SourceAction({
+      actionName: 'Source',
+      bucket: props.s3Stack.bucket,
+      bucketKey: `${resource}/src.zip`,
+      output: sourceOutput,
+      trigger: codepipelineActions.S3Trigger.EVENTS,
+    });
+
+    // Add source action to the pipeline
     pipeline.addStage({
       stageName: 'Source',
       actions: [sourceAction],
     });
 
-    // Add Approve stage
+    // Add Manual Approval stage
     const approveAction = new codepipelineActions.ManualApprovalAction({
-      actionName: 'Manual_Approval',
-      // notifyEmails: ['your.email@example.com'], // Notify this email when manual approval is required
+      actionName: 'ManualApproval',
     });
 
     pipeline.addStage({
@@ -92,10 +77,10 @@ export class CodepipelineBuildDeployStack extends cdk.Stack {
     });
 
     // Define build project
-    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
+    const buildProject = new codebuild.PipelineProject(this, `${resource}BuildProject`, {
       environment: {
         buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
-        privileged: true, // Required to build Docker images
+        privileged: true,
       },
       buildSpec: codebuild.BuildSpec.fromObject({
         version: '0.2',
@@ -124,7 +109,7 @@ export class CodepipelineBuildDeployStack extends cdk.Stack {
               'echo Pushing the Docker images...',
               'docker push $REPOSITORY_URI:latest',
               'docker push $REPOSITORY_URI:$IMAGE_TAG',
-              'printf \'[{"name":"web","imageUri":"%s"}]\' $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json',
+              `printf '[{"name":"${resource}","imageUri":"%s"}]' $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json`,
             ],
           },
         },
@@ -142,37 +127,60 @@ export class CodepipelineBuildDeployStack extends cdk.Stack {
     // Define build action
     const buildOutput = new codepipeline.Artifact();
     const buildAction = new codepipelineActions.CodeBuildAction({
-      actionName: 'Build',
+      actionName: `${resource}Build`,
       project: buildProject,
       input: sourceOutput,
-      outputs: [buildOutput], // Define output artifacts
+      outputs: [buildOutput],
     });
 
     // Add build stage to the pipeline
     pipeline.addStage({
-      stageName: 'Build',
+      stageName: `${resource}Build`,
       actions: [buildAction],
     });
 
+    // Define the service variable based on resource
+    let service;
+    switch(resource) {
+      case 'api':
+        service = props.service?.apiService;
+        break;
+      case 'web':
+        service = props.service?.webService;
+        break;
+      case 'admin':
+        service = props.service?.adminService;
+        break;
+      default:
+        throw new Error(`Unsupported resource: ${resource}`);
+    }
     // Define deploy action (example deployment to ECS)
     const deployAction = new codepipelineActions.EcsDeployAction({
-      actionName: 'DeployAction',
-      service: props.service,
-      input: buildOutput,
+      actionName: `${resource}DeployAction`,
+      service: service.service, // ECS service name determined dynamically
+      input: buildOutput, // Assuming buildOutput is your pipeline's build output artifact
     });
 
     // Add deploy stage to the pipeline
     pipeline.addStage({
-      stageName: 'Deploy',
+      stageName: `${resource}Deploy`,
       actions: [deployAction],
     });
 
     // Grant pipeline role necessary permissions
-    props.s3Bucket.grantReadWrite(pipeline.role);
     props.ecrRepo.grantPullPush(buildProject.role!);
     buildProject.addToRolePolicy(new iam.PolicyStatement({
-      actions: ['ecr:GetDownloadUrlForLayer', 'ecr:BatchGetImage', 'ecr:CompleteLayerUpload', 'ecr:UploadLayerPart', 'ecr:InitiateLayerUpload'],
-      resources: [`${props.ecrRepo.repositoryArn}`],
+      actions: [
+        'ecr:GetDownloadUrlForLayer',
+        'ecr:BatchGetImage',
+        'ecr:CompleteLayerUpload',
+        'ecr:UploadLayerPart',
+        'ecr:InitiateLayerUpload',
+      ],
+      resources: [props.ecrRepo.repositoryArn],
     }));
+  
+    // Grant S3 bucket permissions
+    props.s3Stack.bucket.grantReadWrite(pipeline.role);
   }
 }
--- lib/codepipeline/codepipelineCodeCommit.ts
+++ lib/codepipeline/codepipelineCodeCommit.ts
@@ -10,12 +10,16 @@ import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import { StackProps } from 'aws-cdk-lib';
+import { S3Stack } from '../s3-stack';
+import { ECSStack } from '../cluster-stack';
+import { EC2Stack } from '../ec2-stack';
 
 export interface CodepipelineStackProps extends StackProps {
-  s3Bucket?: s3.Bucket,
-  ecrRepo?: ecr.Repository,
-  service?: ecs.FargateService,
-  codeRepo?: codecommit.Repository
+  s3Stack?: S3Stack;
+  ec2?: EC2Stack;
+  ecrRepo?: ecr.Repository;
+  service?: ECSStack;
+  resource?: ('api' | 'web' | 'admin');
 }
 
 
@@ -23,22 +27,37 @@ export class CodePipelineCodeCommitStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: CodepipelineStackProps) {
     super(scope, id, props);
 
+    // Ensure props.resource is defined and not empty
+    const resource = props.resource ?? 'web';
+
     // Create S3 bucket if not provided
-    if (props.s3Bucket == undefined) {
-      props.s3Bucket = new s3.Bucket(this, 'SourceBucket', {
-        versioned: true, // Enable versioning for the bucket
-      });
+    if (props.s3Stack == undefined) {
+      props.s3Stack = new S3Stack(this);
     }
+
+    // Create an ECR repository if not provided
+    if (props.ecrRepo == undefined) {
+      props.ecrRepo = new ecr.Repository(this, 'EcrRepository');
+    }
+    
+    // Create EC2 stack if not provided
+    if (props.ec2 == undefined) {
+      props.ec2 = new EC2Stack(this);
+    }
+    
+    // Create ECS service if not provided
+    if (props.service == undefined) {
+      props.service = new ECSStack(this, props.ec2, props.s3Stack);
+    }
+
 
     // Create CodeCommit repository
-    if (props.codeRepo == undefined){
-      props.codeRepo = new codecommit.Repository(this, 'CodeRepo', {
-        repositoryName: 'MyCodeRepo',
-      });
-    }
-
+    const codeRepo =  new codecommit.Repository(this, `${resource}-CodeRepo`, {
+      repositoryName: `${resource}-Repo`,
+    });
+      
     // Create CodeBuild project to push code to S3
-    const pushToS3Project = new codebuild.PipelineProject(this, 'PushToS3Project', {
+    const pushToS3Project = new codebuild.PipelineProject(this, `${resource}-PushToS3Project`, {
       buildSpec: codebuild.BuildSpec.fromObject({
         version: '0.2',
         phases: {
@@ -49,7 +68,7 @@ export class CodePipelineCodeCommitStack extends cdk.Stack {
           },
           build: {
             commands: [
-              'aws s3 cp src.zip s3://$S3_BUCKET/src.zip',
+              `aws s3 cp src.zip s3://$S3_BUCKET/${resource}/src.zip`,
             ],
           },
         },
@@ -60,84 +79,39 @@ export class CodePipelineCodeCommitStack extends cdk.Stack {
       }),
       environmentVariables: {
         'S3_BUCKET': {
-          value: props.s3Bucket.bucketName,
+          value: props.s3Stack.bucket.bucketName,
         },
       },
     });
 
     // Grant necessary permissions to CodeBuild project
-    props.s3Bucket.grantReadWrite(pushToS3Project);
-    props.codeRepo.grantRead(pushToS3Project);
-
-    // Create an ECR repository if not Provided
-    if (props.ecrRepo == undefined){
-      props.ecrRepo = new ecr.Repository(this, 'EcrRepository');
-    }
-   
-    //Create ECS Service
-    if (props.service == undefined){
-      // Create ECS cluster
-      const cluster = new ecs.Cluster(this, 'EcsCluster', {
-        vpc: new ec2.Vpc(this, 'Vpc', { maxAzs: 3 }) // Default is all AZs in the region
-      });
-
-      // Create ECS task definition
-      const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef');
-      const container = taskDefinition.addContainer('web', {
-        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
-        memoryLimitMiB: 512,
-        cpu: 256,
-        logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'ecs' }),
-      });
-      container.addPortMappings({
-        containerPort: 80,
-      });
-
-      // Create ECS service
-      props.service = new ecs.FargateService(this, 'Service', {
-        cluster,
-        taskDefinition,
-        desiredCount: 1,
-      });                           
-    }
-
+    props.s3Stack.bucket.grantReadWrite(pushToS3Project);
+    codeRepo.grantRead(pushToS3Project);
 
     // Create CodePipeline
-    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
-      pipelineName: "dev",
+    const pipeline = new codepipeline.Pipeline(this, '${resource}-Pipeline', {
+      pipelineName: '${resource}dev',
     });
 
-    // Add CodeCommit source action
+    // Add source action (S3 event)
     const sourceOutput = new codepipeline.Artifact();
-    const codeCommitSourceAction = new codepipelineActions.CodeCommitSourceAction({
-      actionName: 'CodeCommit_Source',
-      repository: props.codeRepo,
-      branch: 'main',
+    const sourceAction = new codepipelineActions.S3SourceAction({
+      actionName: 'Source',
+      bucket: props.s3Stack.bucket,
+      bucketKey: '${resource}/src.zip',
       output: sourceOutput,
+      trigger: codepipelineActions.S3Trigger.EVENTS,
     });
 
-    // Add the source action to the pipeline
+    // Add source action to the pipeline
     pipeline.addStage({
       stageName: 'Source',
-      actions: [codeCommitSourceAction],
+      actions: [sourceAction],
     });
 
-    // Add build stage to push source to S3
-    const pushToS3Action = new codepipelineActions.CodeBuildAction({
-      actionName: 'PushToS3',
-      project: pushToS3Project,
-      input: sourceOutput,
-    });
-
-    pipeline.addStage({
-      stageName: 'PushToS3',
-      actions: [pushToS3Action],
-    });
-
-    // Add Approve stage
+    // Add Manual Approval stage
     const approveAction = new codepipelineActions.ManualApprovalAction({
-      actionName: 'Manual_Approval',
-      // notifyEmails: ['your.email@example.com'], // Notify this email when manual approval is required
+      actionName: 'ManualApproval',
     });
 
     pipeline.addStage({
@@ -145,88 +119,115 @@ export class CodePipelineCodeCommitStack extends cdk.Stack {
       actions: [approveAction],
     });
 
-    // Define build project for Docker image
-    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
-        privileged: true, // Required to build Docker images
-      },
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: '0.2',
-        phases: {
-          pre_build: {
-            commands: [
-              'echo Logging in to Amazon ECR...',
-              'aws --version',
-              '$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)',
-              'REPOSITORY_URI=$REPOSITORY_URI',
-              'COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)',
-              'IMAGE_TAG=${COMMIT_HASH:=latest}',
-            ],
+      // Define build project
+      const buildProject = new codebuild.PipelineProject(this, `${resource}BuildProject`, {
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+          privileged: true,
+        },
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: '0.2',
+          phases: {
+            pre_build: {
+              commands: [
+                'echo Logging in to Amazon ECR...',
+                'aws --version',
+                '$(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)',
+                'REPOSITORY_URI=$REPOSITORY_URI',
+                'COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)',
+                'IMAGE_TAG=${COMMIT_HASH:=latest}',
+              ],
+            },
+            build: {
+              commands: [
+                'echo Build started on `date`',
+                'echo Building the Docker image...',
+                'docker build -t $REPOSITORY_URI:latest .',
+                'docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG',
+              ],
+            },
+            post_build: {
+              commands: [
+                'echo Build completed on `date`',
+                'echo Pushing the Docker images...',
+                'docker push $REPOSITORY_URI:latest',
+                'docker push $REPOSITORY_URI:$IMAGE_TAG',
+                `printf '[{"name":"${resource}","imageUri":"%s"}]' $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json`,
+              ],
+            },
           },
-          build: {
-            commands: [
-              'echo Build started on `date`',
-              'echo Building the Docker image...',
-              'docker build -t $REPOSITORY_URI:latest .',
-              'docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG',
-            ],
+          artifacts: {
+            files: 'imagedefinitions.json',
           },
-          post_build: {
-            commands: [
-              'echo Build completed on `date`',
-              'echo Pushing the Docker images...',
-              'docker push $REPOSITORY_URI:latest',
-              'docker push $REPOSITORY_URI:$IMAGE_TAG',
-              'printf \'[{"name":"web","imageUri":"%s"}]\' $REPOSITORY_URI:$IMAGE_TAG > imagedefinitions.json',
-            ],
+        }),
+        environmentVariables: {
+          'REPOSITORY_URI': {
+            value: props.ecrRepo.repositoryUri,
           },
         },
-        artifacts: {
-          files: 'imagedefinitions.json',
-        },
-      }),
-      environmentVariables: {
-        'REPOSITORY_URI': {
-          value: props.ecrRepo.repositoryUri,
-        },
-      },
-    });
+      });
 
-    // Define build action for Docker image
-    const buildOutput = new codepipeline.Artifact();
-    const buildAction = new codepipelineActions.CodeBuildAction({
-      actionName: 'Build',
-      project: buildProject,
-      input: sourceOutput,
-      outputs: [buildOutput], // Define output artifacts
-    });
+      // Define build action
+      const buildOutput = new codepipeline.Artifact();
+      const buildAction = new codepipelineActions.CodeBuildAction({
+        actionName: `${resource}Build`,
+        project: buildProject,
+        input: sourceOutput,
+        outputs: [buildOutput],
+      });
 
-    // Add build stage for Docker image
-    pipeline.addStage({
-      stageName: 'Build',
-      actions: [buildAction],
-    });
+      // Add build stage to the pipeline
+      pipeline.addStage({
+        stageName: `${resource}Build`,
+        actions: [buildAction],
+      });
 
-    // Define deploy action (example deployment to ECS)
-    const deployAction = new codepipelineActions.EcsDeployAction({
-      actionName: 'DeployAction',
-      service: props.service,
-      input: buildOutput,
-    });
+      let service;
+      switch(resource) {
+        case 'api':
+          service = props.service?.apiService;
+          break;
+        case 'web':
+          service = props.service?.webService;
+          break;
+        case 'admin':
+          service = props.service?.adminService;
+          break;
+        default:
+          throw new Error(`Unsupported resource: ${resource}`);
+      }
+      // Define deploy action (example deployment to ECS)
+      const deployAction = new codepipelineActions.EcsDeployAction({
+        actionName: `${resource}DeployAction`,
+        service: service.service, // ECS service name determined dynamically
+        input: buildOutput, // Assuming buildOutput is your pipeline's build output artifact
+      });
 
-    // Add deploy stage to the pipeline
-    pipeline.addStage({
-      stageName: 'Deploy',
-      actions: [deployAction],
-    });
+      // Add deploy stage to the pipeline
+      pipeline.addStage({
+        stageName: `${resource}Deploy`,
+        actions: [deployAction],
+      });
 
-    // Grant pipeline role necessary permissions
-    props.s3Bucket.grantReadWrite(pipeline.role);
-    props.ecrRepo.grantPullPush(buildProject.role!);
-    buildProject.addToRolePolicy(new iam.PolicyStatement({
-      actions: ['ecr:GetDownloadUrlForLayer', 'ecr:BatchGetImage', 'ecr:CompleteLayerUpload', 'ecr:UploadLayerPart', 'ecr:InitiateLayerUpload'],
-      resources: [`${props.ecrRepo.repositoryArn}`],
-    }));
+      // Grant pipeline role necessary permissions
+      props.ecrRepo.grantPullPush(buildProject.role!);
+      buildProject.addToRolePolicy(new iam.PolicyStatement({
+        actions: [
+          'ecr:GetDownloadUrlForLayer',
+          'ecr:BatchGetImage',
+          'ecr:CompleteLayerUpload',
+          'ecr:UploadLayerPart',
+          'ecr:InitiateLayerUpload',
+        ],
+        resources: [props.ecrRepo.repositoryArn],
+      }));
+
+      // Grant pipeline role necessary permissions
+      props.s3Stack?.bucket.grantReadWrite(pipeline.role);
+      props.ecrRepo.grantPullPush(buildProject.role!);
+      buildProject.addToRolePolicy(new iam.PolicyStatement({
+        actions: ['ecr:GetDownloadUrlForLayer', 'ecr:BatchGetImage', 'ecr:CompleteLayerUpload', 'ecr:UploadLayerPart', 'ecr:InitiateLayerUpload'],
+        resources: [`${props.ecrRepo.repositoryArn}`],
+      }));
   }
 }
