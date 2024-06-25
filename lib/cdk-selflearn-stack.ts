6c9376fdf660c65cd277db77efb35908e82bfcc7
doletu <102934755+vfa-tudl@users.noreply.github.com>
2024-06-25 22:37:39 +0700

    test/Deploy Image

--- lib/cdk-selflearn-stack.ts
+++ lib/cdk-selflearn-stack.ts
@@ -9,6 +9,7 @@ import { CloudfrontStack } from './cloudfront-stack';
 import { AcmStack } from './acm-stack';
 import { Route53Stack } from './route53-stack';
 import { AlbStack } from './alb-stack';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkSelflearnStack extends cdk.Stack {
@@ -18,20 +19,21 @@ export class CdkSelflearnStack extends cdk.Stack {
 
     const s3 = new S3Stack(this);
     const ec2 = new EC2Stack(this);
+
     const route53 = new Route53Stack(this);
     const acm = new AcmStack(this, route53);
     const cloudfront = new CloudfrontStack(this, s3, acm);
     const targetCloudfrontRecord = Route53Stack.createCloudfrontTargetRecord(
       cloudfront.distribution
     );
-    route53.addARecord(targetCloudfrontRecord);
-    route53.addAaaaRecord(targetCloudfrontRecord);
+    route53.addARecord(targetCloudfrontRecord,'CloudfrontARecord');
+    route53.addAaaaRecord(targetCloudfrontRecord,'CloudfrontAAAARecord');
     const alb = new AlbStack(this, ec2);
     const ecs = new ECSStack(this, ec2, s3, alb);
     const targetAlbRecord = Route53Stack.createAlbTargetRecord(
       alb.alb
     );
-    route53.addARecord(targetAlbRecord);
-    route53.addAaaaRecord(targetAlbRecord);
+    route53.addARecord(targetAlbRecord,'AlbARecord');
+    route53.addAaaaRecord(targetAlbRecord,'AlbAAAARecord');
   }
 }
--- lib/route53-stack.ts
+++ lib/route53-stack.ts
@@ -1,10 +1,12 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as route53 from 'aws-cdk-lib/aws-route53';
-import * as targets from 'aws-cdk-lib/aws-route53-targets'
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as logs from 'aws-cdk-lib/aws-logs';
-import getEnv from '../shared/getEnv';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import { ILoadBalancerV2 } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import getEnv from '../shared/getEnv';
+import * as cdk from 'aws-cdk-lib';
 
 export class Route53Stack {
   private readonly stackScope: Construct;
@@ -15,7 +17,40 @@ export class Route53Stack {
     const logGroup = new logs.LogGroup(scope, 'cdk-route53-log-group');
     const zoneName = getEnv('ROUTE53_ZONE_NAME');
     console.log(zoneName);
-    if (!zoneName) return;
+    if (!zoneName || zoneName.length === 0) {
+      throw new Error('DOMAIN_NAME_LIST environment variable is not set or is empty');
+    }
+    logGroup.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    const logGroupPolicy = new logs.CfnResourcePolicy(scope, 'LogGroupResourcePolicy', {
+      policyName: 'Route53QueryLoggingPolicy',
+      policyDocument: JSON.stringify({
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Principal: {
+              Service: 'route53.amazonaws.com',
+            },
+            Action:  [
+              "logs:CreateLogStream",
+              "logs:PutLogEvents",
+              "logs:PutLogEventsBatch",
+            ],
+            Resource: logGroup.logGroupArn,
+          },
+          {
+            Effect: 'Allow',
+            Principal: {
+              Service: 'route53.amazonaws.com',
+            },
+            Action: 'logs:CreateLogStream',
+            Resource: `${logGroup.logGroupArn}:log-stream:*`,
+          },
+        ],
+      }),
+    });
+
+    logGroupPolicy.node.addDependency(logGroup);
     this.publicHostedZone = new route53.PublicHostedZone(scope, 'cdk-route53-public-hosted-zone', {
       zoneName,
       queryLogsLogGroupArn: logGroup.logGroupArn,
@@ -26,17 +61,19 @@ export class Route53Stack {
    * Add A record target to route 53
    * @param target Record target
    */
-  public addARecord(target: route53.IAliasRecordTarget) {
-    new route53.ARecord(this.stackScope, 'cdk-alias-a-record-cloudfront', {
+  public addARecord(target: route53.IAliasRecordTarget, id: string) {
+    new route53.ARecord(this.stackScope, id, {
       zone: this.publicHostedZone,
-      target: route53.RecordTarget.fromAlias(target)
+      target: route53.RecordTarget.fromAlias(target),
+      recordName: id
     })
   }
 
-  public addAaaaRecord(target: route53.IAliasRecordTarget) {
-    new route53.AaaaRecord(this.stackScope, 'cdk-alias-aaaa-record-cloudfront', {
+  public addAaaaRecord(target: route53.IAliasRecordTarget, id: string) {
+    new route53.ARecord(this.stackScope, id, {
       zone: this.publicHostedZone,
-      target: route53.RecordTarget.fromAlias(target)
+      target: route53.RecordTarget.fromAlias(target),
+      recordName: id
     })
   }
 
